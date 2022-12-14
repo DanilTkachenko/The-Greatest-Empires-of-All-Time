08bb292d1ca7400120c78810012a92c2a6e0cac0
Danil Tkachenko <tkachenkodanil06@gmail.com>
2022-12-14 17:23:56 +0200

    Main block with minimal functionality

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let clicks = document.querySelectorAll('.empire__name');
+
+for (let i = 0; i < clicks.length; i++) {
+	clicks[i].onclick = function () {
+		if (this.parentNode.classList) {
+			for (let j = 0; j < clicks.length; j++) {
+				clicks[j].parentNode.classList.remove('active');
+			}
+			this.parentNode.classList.add('active');
+		} else {
+			this.active += ' ' + active;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const arrows = document.querySelectorAll('.empire__name');
+
+for (const arrow of arrows) {
+	arrow.addEventListener('click', function () {
+		arrow.parentNode.classList.toggle('active');
+
+		if (!document.querySelector('.empires__content').classList.contains('active')) {
+			arrow.parentNode.parentNode.parentNode.classList.toggle('active');
+		} else {
+			return;
+		}
+	});
+}
+*/
