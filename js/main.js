b13df9a5bc0a7962a481cc162215e9c3a6f90ede
Danil Tkachenko <tkachenkodanil06@gmail.com>
2022-12-14 18:45:23 +0200

    Add animation to main block

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let clicks = document.querySelectorAll('.empire__name');
 
 for (let i = 0; i < clicks.length; i++) {
 	clicks[i].onclick = function () {
-		if (this.parentNode.classList) {
+		if (this.parentNode.parentNode.classList) {
 			for (let j = 0; j < clicks.length; j++) {
-				clicks[j].parentNode.classList.remove('active');
+				clicks[j].parentNode.parentNode.classList.remove('active');
 			}
-			this.parentNode.classList.add('active');
+			this.parentNode.parentNode.classList.add('active');
 		} else {
 			this.active += ' ' + active;
 		}
